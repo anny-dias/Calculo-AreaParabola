b52134a3ccb37918c556a29dfdd7634a1084f16b
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-18 09:18:00 -0300

    calculando area

---
+++ exemplo.py
@@ -0,0 +1,23 @@
+"""Calculando a area de uma parabola"""
+
+
+def funcao(x):
+    y = (-x**2) + (25 * x)
+    return y
+
+  
+x = 0 
+delta = 0.01
+Area = 0
+
+while x < 25:
+    f = funcao(x)
+    area = f * delta
+    Area += area
+    x += delta
+print(Area)
+
+
+
+
+    
